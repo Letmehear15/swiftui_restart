97822adec66546ced14057703b6a9aa97db3c4f0
alex <aleksandr.nesterov@notino.com>
2024-08-29 19:54:40 +0200

    Initial Commit

---
+++ restart/restartApp.swift
@@ -0,0 +1,17 @@
+//
+//  restartApp.swift
+//  restart
+//
+//  Created by Aleksandr Nesterov on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct restartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
